19d1671baf4d8dafa0e958d76c59693a3279b7fc
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-05-26 17:22:27 +0200

    somma vers. 2

--- pariedispari/script.js
+++ pariedispari/script.js
@@ -1,15 +1,13 @@
 //const userInput1 = prompt("scegli pari o dispari");
 const userInput2 = parseInt(prompt("scegli un numero da 1 a 5"));
 
+//funzione per genrare un numero random da 1 a 5 per il pc
 function pcNumeroRandom (){
     const numeroRandom = Math.floor(Math.random() * 5)+1;
     return numeroRandom;
 }
 
-let numeroGeneratoPc = pcNumeroRandom();
+//console.log(pcNumeroRandom());
 
-console.log(pcNumeroRandom());
-
-let sommaNumeri = userInput2 + numeroGeneratoPc;
-
+let sommaNumeri=userInput2+pcNumeroRandom();
 console.log(sommaNumeri);
