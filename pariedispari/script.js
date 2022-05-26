2750379dc22508dfe005ba029ae8d113b3768582
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-05-26 18:11:50 +0200

    pariedispari test pt5

--- pariedispari/script.js
+++ pariedispari/script.js
@@ -1,4 +1,4 @@
-//const userInput1 = prompt("scegli pari o dispari");
+const userInput1 = prompt("scegli pari o dispari");
 const userInput2 = parseInt(prompt("scegli un numero da 1 a 5"));
 
 //funzione per generare un numero random da 1 a 5 per il pc
@@ -28,7 +28,13 @@ function sommaTraPariDispari (num1, num2){
     }
 }
 
+let verificaPariDispari = sommaTraPariDispari(userInput2, numeroGeneratoPc);
+
 console.log("l'utente ha inserito il numero " + userInput2);
 console.log("il numero generato per il pc è " + numeroGeneratoPc);
-console.log(sommaTraPariDispari(userInput2, numeroGeneratoPc));
+console.log(verificaPariDispari);
 
+/* if(userInput2 == "pari" && verificaPariDispari){
+
+}
+ */
