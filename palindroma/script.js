8790ef8942107a8e3298455b4ba1136ba2881f5d
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-05-26 15:13:54 +0200

    test palindromo pt.2

--- palindroma/script.js
+++ palindroma/script.js
@@ -1,12 +1,26 @@
 let inputParola = prompt("inserisci una parola");
 
 
-function verificaPalindromo (parola){
+/* function verificaPalindromo (parola){
     let parolaScomposta = [];
 
     parolaScomposta.push(parola.split(""));
     
     return parolaScomposta;
+}  */
+function verificaPalindromo (parola){
+    /* let parolaScompostaOrdered = [];
+    for (let i=0; i<parola.length; i++){
+        parolaScompostaOrdered.push(parola[i]);
+    } */
+    let parolaScompostaOpposite = [];
+    for (let i=0;i<parola.length; i++){
+        parolaScompostaOpposite.push(parola[i]);
+        parolaScompostaOpposite.reverse(i);
+    }
+
+    //return parolaScompostaOrdered;
+    return parolaScompostaOpposite;
 } 
 
 console.log(verificaPalindromo(inputParola));
